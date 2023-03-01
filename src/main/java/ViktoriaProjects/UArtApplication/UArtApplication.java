aa25d5d4059b1754c18b59ada2c265f8d0370f93
VictoriaShevliuk <viktoriasvyatova37@gmail.com>
2023-03-01 21:19:37 +0200

    Initialization of the project

---
+++ src/main/java/ViktoriaProjects/UArtApplication/UArtApplication.java
@@ -0,0 +1,13 @@
+package ViktoriaProjects.UArtApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UArtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UArtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ViktoriaProjects/UArtApplication/UArtApplicationTests.java
@@ -0,0 +1,13 @@
+package ViktoriaProjects.UArtApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UArtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
