494dd6d6596af688a5ce8aba91774dc13a9f36c5
VictoriaShevliuk <viktoriasvyatova37@gmail.com>
2023-03-03 17:16:05 +0200

    UserService modified

--- src/main/java/ViktoriaProjects/UArtApplication/service/impl/UserServiceImpl.java
+++ src/main/java/ViktoriaProjects/UArtApplication/service/impl/UserServiceImpl.java
@@ -1,26 +1,69 @@
 package ViktoriaProjects.UArtApplication.service.impl;
 
 import ViktoriaProjects.UArtApplication.model.User;
+import ViktoriaProjects.UArtApplication.repository.UserRepository;
 import ViktoriaProjects.UArtApplication.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PostFilter;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.access.prepost.PreFilter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@Service
+@Service("userServiceImpl")
 
 public class UserServiceImpl implements UserService {
-    @Autowired
     private UserRepository userRepository;
 
+    private final PasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(username);
-
+    public User create(User user) {
         if (user != null) {
-            return user;
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            return userRepository.save(user);
         }
+        throw new NullEntityReferenceException("User cannot be 'null'");
+    }
 
-        throw new UsernameNotFoundException("user not found with username: " + username);
+    @Override
+    @PostAuthorize("hasRole('ROLE_ADMIN') or returnObject.getEmail() == authentication.name")
+    public User readById(long id) {
+        return userRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("User with id " + id + " not found"));
+    }
+
+    @Override
+    public User update(User role) {
+        if (role != null) {
+            readById(role.getId());
+            return userRepository.save(role);
+        }
+        throw new NullEntityReferenceException("User cannot be 'null'");
+    }
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public void delete(long id) {
+        userRepository.delete(readById(id));
+    }
+
+    @Override
+    @PostFilter("hasRole('ROLE_ADMIN') or filterObject.getEmail() == authentication.name")
+    public List<User> getAll() {
+        List<User> users = userRepository.findAll();
+        return users.isEmpty() ? new ArrayList<>() : users;
     }
 }
